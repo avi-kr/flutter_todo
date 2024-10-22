6c53bdbbce02f1fd90ff7b55a451d6830bd76946
Abhishek Kumar <avi.kr16@gmail.com>
2024-10-22 17:52:29 +0530

    Todo Row tile Created
    Signed-off-by: Abhishek Kumar <avi.kr16@gmail.com>

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["ToDo Task 1", true],
+    ["ToDo Task 2", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +27,16 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("To Do"),
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            isTaskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
